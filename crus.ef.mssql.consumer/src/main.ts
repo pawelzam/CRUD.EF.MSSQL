dcb6d6d6998d546f448e239d3f631e5062728bf7
Paweł Zamrij <_PZamrij.AZ@sodexo.com>
2024-10-08 13:54:19 +0200

    Fix

--- crus.ef.mssql.consumer/src/main.ts
+++ crus.ef.mssql.consumer/src/main.ts
@@ -1,10 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(HttpClientModule)
+    provideHttpClient()
   ]
 }).catch(err => console.error(err));
